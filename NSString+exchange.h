f384a28179c0362ddfe2f7ed8fde025d256326d4
Irisgh <irisg80236@163.com>
2019-01-17 16:10:04 +0800

    添加分类

---
+++ NSString+exchange.h
@@ -0,0 +1,19 @@
+//
+//  NSString+exchange.h
+//  mao
+//
+//  Created by 卡德 on 2019/1/17.
+//  Copyright © 2019 卡德. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (exchange)
+
+- (void)exchangeWith:(NSString*)replaceString;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NSString+exchange.m
@@ -0,0 +1,17 @@
+//
+//  NSString+exchange.m
+//  mao
+//
+//  Created by 卡德 on 2019/1/17.
+//  Copyright © 2019 卡德. All rights reserved.
+//
+
+#import "NSString+exchange.h"
+
+@implementation NSString (exchange)
+
+- (void)exchangeWith:(NSString*)replaceString {
+  NSLog(@"%@",replaceString);
+}
+
+@end
